01f1a34b10a91364b1920013ad0263933e716795
kavishdev <46088622+kavishdev@users.noreply.github.com>
2019-12-19 10:22:31 +0530

    Update Rock, Paper, Scissors.py

--- Python/Rock, Paper, Scissors.py
+++ Python/Rock, Paper, Scissors.py
@@ -1,42 +1,42 @@
 from random import randint
+for play in range (5) :
+  player = input('rock(r),paper(p)scissors(s)')
 
-player = input('rock(r),paper(p)scissors(s)')
+  print(player, 'vs', end =' ') 
 
-print(player, 'vs', end =' ') 
+  chosen = randint(1,3)
+  #print(chosen)
 
-chosen = randint(1,3)
-#print(chosen)
+  if chosen == 1:
+    computer = 'r'
 
-if chosen == 1:
-  computer = 'r'
+  elif chosen == 2:
+    computer = 'p'
+  elif chosen == 3:
+    computer = 's'
 
-elif chosen == 2:
-  computer = 'p'
-elif chosen == 3:
-  computer = 's'
-  
-print(computer)
+  print(computer)
 
-if player == computer:
-    print('DRAW!!!!!')
+  if player == computer:
+      print('DRAW!!!!!')
 
-elif player == 'r'and computer == 's':
-        print ('Player wins!!!!!')
+  elif player == 'r'and computer == 's':
+          print ('Player wins!!!!!')
 
-elif computer == 'p' and player == 'r':
-        print ('Computer wins!!!!')
+  elif computer == 'p' and player == 'r':
+          print ('Computer wins!!!!')
 
-elif player == 'p' and computer == 'r':
-     
-elif computer == 's'and player == 'p':
-         print('Computer wins!!!!')
+  elif player == 'p' and computer == 'r':
 
-elif player == 's' and computer == 'p':
-         print('Player wins!!!!')
-     
-elif computer == 's'and player == 'p':
-         print('Computer wins!!!!')
-       
+  elif computer == 's'and player == 'p':
+           print('Computer wins!!!!')
+
+  elif player == 's' and computer == 'p':
+           print('Player wins!!!!')
+
+  elif computer == 's'and player == 'p':
+           print('Computer wins!!!!')
+
 
 
  
